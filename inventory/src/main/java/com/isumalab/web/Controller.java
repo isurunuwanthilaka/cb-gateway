4ff6d2b22b452e7e17e43d4520f78477a2137022
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2020-12-22 14:35:19 +0530

    fix gateway routing

--- inventory/src/main/java/com/isumalab/web/Controller.java
+++ inventory/src/main/java/com/isumalab/web/Controller.java
@@ -1,20 +1,14 @@
 package com.isumalab.web;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+@RestController
+public class Controller {
 
-@Configuration
-public class RouterConfig {
-
-    @Bean
-    public RouterFunction inventoryRoute(InventoryHandler inventoryHandler){
-        return route(GET("/inventory-failover").and(accept(MediaType.APPLICATION_JSON)),
-                inventoryHandler::listInventory);
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
     }
+
 }
